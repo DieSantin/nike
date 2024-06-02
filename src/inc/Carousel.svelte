f93d851caaf18bc72575e1f19da79724b9e361cb
DiegoSantin <diegoemanuelesantin@gmail.com>
2024-06-02 23:21:31 +0200

    componente carousel quasi pronto, per adesso fatto il primo,  da rivedere per essere completamente riutilizzabile

--- src/inc/Carousel.svelte
+++ src/inc/Carousel.svelte
@@ -1,32 +1,13 @@
 <script>
+    export let slides;
+    export let slidesTitle;
+
     let currentIndex = 1;
     let isUserScrolling = 1;
+    let scrollTimeout;
 
     $: clickAbleLeft = currentIndex == 1 ? 0 : 1;
     $: clickAbleRight = currentIndex == 2 ? 0 : 1;
-
-    const slides = [
-        {
-            id: 0,
-            url: "./img/nike1.avif",
-            text: "Scopri Dynamic Air",
-        },
-        {
-            id: 1,
-            url: "./img/nike2.avif",
-            text: "Mad brilliance Pack",
-        },
-        {
-            id: 2,
-            url: "./img/nike3.avif",
-            text: "Novità: tennis",
-        },
-        {
-            id: 3,
-            url: "./img/nike4.avif",
-            text: "Articoli per il fitness",
-        },
-    ];
 
     function index(direction) {
         isUserScrolling = 0;
@@ -42,9 +23,11 @@
         const currentSlide = document.getElementById(`slide-${currentIndex}`);
         currentSlide.scrollIntoView({ behavior: "smooth", inline: "center" });
 
-        setTimeout(() => {
+        if (scrollTimeout) clearTimeout(scrollTimeout);
+
+        scrollTimeout = setTimeout(() => {
             isUserScrolling = 1;
-        }, 1000);
+        }, 1200);
     }
 
     function handleScroll(event) {
@@ -56,33 +39,40 @@
     }
 </script>
 
-<div class="flex items-center justify-between px-2">
-    <div class="text-4xl font-medium py-5">Tutte le migliori novità</div>
-    <div class="flex space-x-7">
-        <button
-            class={` w-14 h-14 rounded-full flex justify-center items-center cursor-default ${
-                clickAbleLeft
-                    ? "bg-gray-200 hover:cursor-pointer hover:bg-gray-300 text-gray-700"
-                    : "bg-gray-50 hover:cursor-default text-gray-300"
-            }`}
-            on:click={() => index("toLeft")}
-        >
-            <span class="material-symbols-sharp text-2xl"> chevron_left </span>
-        </button>
+{#if slides.length > 3}
+    <div class="flex items-center justify-between px-2">
+        <div class="text-4xl font-medium py-5">{slidesTitle}</div>
+        <div class="flex space-x-7">
+            <button
+                class={` w-14 h-14 rounded-full flex justify-center items-center cursor-default
+                ${
+                    clickAbleLeft
+                        ? "bg-gray-200 hover:cursor-pointer hover:bg-gray-300 text-gray-700"
+                        : "bg-gray-50 hover:cursor-default text-gray-300"
+                }`}
+                on:click={() => index("toLeft")}
+            >
+                <span class="material-symbols-sharp text-2xl">
+                    chevron_left
+                </span>
+            </button>
 
-        <button
-            class={`w-14 h-14 rounded-full flex justify-center items-center cursor-default
+            <button
+                class={`w-14 h-14 rounded-full flex justify-center items-center cursor-default
             ${
                 clickAbleRight
                     ? "bg-gray-200 hover:cursor-pointer hover:bg-gray-300 text-gray-700"
                     : "bg-gray-50 hover:cursor-default text-gray-300"
             }`}
-            on:click={() => index("toRight")}
-        >
-            <span class="material-symbols-sharp text-2xl"> chevron_right </span>
-        </button>
+                on:click={() => index("toRight")}
+            >
+                <span class="material-symbols-sharp text-2xl">
+                    chevron_right
+                </span>
+            </button>
+        </div>
     </div>
-</div>
+{/if}
 
 <div
     class="flex overflow-x-auto snap-x snap-mandatory"
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,42 @@
 <script>
     import BgContent from "$inc/BgContent.svelte";
-    import MiglioriNovit from "../inc/MiglioriNovità.svelte";
+    import Carousel from "$inc/Carousel.svelte";
+
+    const miglioriNovità = [
+        {
+            title: "Tutte le migliori novità",
+
+            item: [
+                {
+                    id: 0,
+                    url: "./img/nike1.avif",
+                    text: "Scopri Dynamic Air",
+                },
+                {
+                    id: 1,
+                    url: "./img/nike2.avif",
+                    text: "Mad brilliance Pack",
+                },
+                {
+                    id: 2,
+                    url: "./img/nike3.avif",
+                    text: "Novità: tennis",
+                },
+                {
+                    id: 3,
+                    url: "./img/nike4.avif",
+                    text: "Articoli per il fitness",
+                },
+            ],
+        },
+    ];
+
+    let miglioriNovitàItem = miglioriNovità[0].item;
+    let miglioriNovitàTitle = miglioriNovità[0].title;
 </script>
 
 <BgContent />
 
 <div class="px-10 py-10">
-    <MiglioriNovit />
+    <Carousel slides={miglioriNovitàItem} slidesTitle={miglioriNovitàTitle} />
 </div>
